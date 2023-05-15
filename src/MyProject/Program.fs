543d3fdd384da862206e27753b543d27f9fa1333
Flavio Colavecchia <fcolavecchia@gmail.com>
2023-05-15 10:19:01 -0300

    Added reference to library

--- src/MyProject/Program.fs
+++ src/MyProject/Program.fs
@@ -1,2 +1,25 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+// Implicit entry point 
+// printfn "Hello from F#"
+
+
+// Explicit entry poiny
+// [<EntryPoint>]
+// let main args =
+//     printfn "Arguments passed to function : %A" args
+//     // Return 0. This indicates success.
+//     0
+
+// Explicit entry poiny
+
+open MyLibrary 
+
+[<EntryPoint>]
+let main args =
+    
+    let myName = "Flavio"
+    
+    MyLibrary.Say.hello myName 
+    // printfn "Arguments passed to function : %A" args
+    // Return 0. This indicates success.
+    0
